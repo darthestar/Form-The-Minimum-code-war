3bdd9a0d613dcef2d7a4f024faa39e553214f1f4
Sparks <dsparks@proforma.com>
2018-04-25 13:55:18 -0400

    Add project files.

---
+++ Form The Minimum code war/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Form_The_Minimum_code_war
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
