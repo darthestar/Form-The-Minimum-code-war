6d61522d8b9d91b5cf9d92154a46b33fbb761108
Sparks <dsparks@proforma.com>
2018-04-26 12:56:07 -0400

    c# solution

--- Form The Minimum code war/Program.cs
+++ Form The Minimum code war/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace Form_The_Minimum_code_war
 {
+
     class Program
     {
+        //sort
+        //remove dups
+        //push number into []
+        //combine
+
+        public static long MinValue(int[] array)
+        {
+            var removeDups = array.Distinct();
+            foreach (var number in removeDups)
+            {
+
+            };
+            var sort = from s in removeDups orderby s select s;
+            var result = String.Join("", sort);
+            Console.WriteLine(result);
+            var endresult = Int32.Parse(result);
+            return endresult;
+        }
+
         static void Main(string[] args)
         {
+            int[] a = { 6, 7, 8, 7, 6, 6 };
+
+            MinValue(a);
+            Console.ReadLine();
+
         }
     }
 }
+
+
+
+
